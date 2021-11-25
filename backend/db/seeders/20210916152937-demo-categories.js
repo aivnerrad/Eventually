c43cfe3a24e18dbf3a3c4e8eb0c167cee2778635
aivnerrad <aivnerrad1993@gmail.com>
2021-11-24 22:58:37 -0500

    actually fixed dates

--- backend/db/seeders/20210916152937-demo-categories.js
+++ backend/db/seeders/20210916152937-demo-categories.js
@@ -5,32 +5,32 @@ module.exports = {
    return queryInterface.bulkInsert('Categories', [{
      name: "Yard Sale",
      imageUrl: "https://pacresmortgage.com/wp-content/uploads/8.21.20-fam-friendly-1536x1025.jpg",
-     createdAt: new Date(),
-     updatedAt: new Date(),
+     createdAt: new Date(Date.UTC(2021, 11, 20, 4, 0, 0)),
+     updatedAt: new Date(Date.UTC(2021, 11, 20, 4, 0, 0)),
    },
    {
     name: "Garage Sale",
     imageUrl: "https://pacresmortgage.com/wp-content/uploads/8.21.20-fam-friendly-1536x1025.jpg",
-    createdAt: new Date(),
-    updatedAt: new Date(),
+    createdAt: new Date(Date.UTC(2021, 11, 20, 4, 0, 0)),
+    updatedAt: new Date(Date.UTC(2021, 11, 20, 4, 0, 0)),
   },
   {
     name: "Moving Sale",
     imageUrl: "https://pacresmortgage.com/wp-content/uploads/8.21.20-fam-friendly-1536x1025.jpg",
-    createdAt: new Date(),
-    updatedAt: new Date(),
+    createdAt: new Date(Date.UTC(2021, 11, 20, 4, 0, 0)),
+    updatedAt: new Date(Date.UTC(2021, 11, 20, 4, 0, 0)),
   },
   {
     name: "Estate Sale",
     imageUrl: "https://pacresmortgage.com/wp-content/uploads/8.21.20-fam-friendly-1536x1025.jpg",
-    createdAt: new Date(),
-    updatedAt: new Date(),
+    createdAt: new Date(Date.UTC(2021, 11, 20, 4, 0, 0)),
+    updatedAt: new Date(Date.UTC(2021, 11, 20, 4, 0, 0)),
   },
   {
     name: "Flea Market",
     imageUrl: "https://pacresmortgage.com/wp-content/uploads/8.21.20-fam-friendly-1536x1025.jpg",
-    createdAt: new Date(),
-    updatedAt: new Date(),
+    createdAt: new Date(Date.UTC(2021, 11, 20, 4, 0, 0)),
+    updatedAt: new Date(Date.UTC(2021, 11, 20, 4, 0, 0)),
   }], {});
   },
 
