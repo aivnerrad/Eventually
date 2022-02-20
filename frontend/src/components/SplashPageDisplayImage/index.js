df4ce59549b861b705b2c075351f80e9fa7ec891
aivnerrad <aivnerrad1993@gmail.com>
2022-02-20 14:24:08 -0500

    removed some unnecessary css

--- frontend/src/components/SplashPageDisplayImage/index.js
+++ frontend/src/components/SplashPageDisplayImage/index.js
@@ -4,7 +4,6 @@ export default function SplashImage() {
   return (
     <div id="top-image-div">
       <div id="splash-div">
-        <h1 id="intro-text">Today is a <u id="intro-underline">beautiful</u> day!</h1>
       </div>
     </div>
   )
